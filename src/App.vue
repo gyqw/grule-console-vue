c131e0240ae1eb56094dbab1ebc4450ef319c53f
guye <guye@geexfinance.com>
2019-11-20 17:54:32 +0800

    添加axios

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
     <el-container>
         <el-aside width="200px" style="background-color: rgb(238, 241, 246)">
-            <el-menu :default-openeds="['1', '3']">
+            <el-menu :default-openeds="['1']">
                 <el-submenu index="1">
                     <template slot="title"><i class="el-icon-message"></i>导航一</template>
                     <el-menu-item-group>
@@ -64,14 +64,6 @@
             </el-header>
 
             <el-main>
-                <el-table :data="tableData">
-                    <el-table-column prop="date" label="日期" width="140">
-                    </el-table-column>
-                    <el-table-column prop="name" label="姓名" width="120">
-                    </el-table-column>
-                    <el-table-column prop="address" label="地址">
-                    </el-table-column>
-                </el-table>
             </el-main>
         </el-container>
     </el-container>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 import Vue from 'vue'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css';
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
 import App from './App.vue'
 
 Vue.use(ElementUI)
+Vue.use(VueAxios, axios)
 Vue.config.productionTip = false
 
 new Vue({
